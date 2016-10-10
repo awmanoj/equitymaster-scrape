996b788cc23269289f96f9c79b3fe1f5a7797547
Manoj Awasthi <awasthi.manoj@gmail.com>
2016-10-10 22:54:49 +0700

    Add analyze.py improvements.

--- analyze.py
+++ analyze.py
@@ -17,8 +17,11 @@ cmap = dict()
 for co in doc["Companies"]["Company"]:
 	cmap[co["Symbol"]] = co["DispName"]
 
-file = open("report.txt")
-with open("report.txt") as file:
+report = "report.txt"
+if len(sys.argv) > 3:
+	report = sys.argv[3]
+
+with open(report) as file:
 	for line in file:
 		splits = line.strip().split("|")
 		stock_code = splits[0]
@@ -26,7 +29,7 @@ with open("report.txt") as file:
 		ders = map(lambda x: float(x), splits[2].split(","))
 	
 		skip = False
-		for roe in roes:
+		for roe in roes[:-1]:
 			if roe < min_roe:
 				skip = True
 				break
@@ -34,7 +37,7 @@ with open("report.txt") as file:
 		if skip: continue	
 
 		skip = False
-		for der in ders:
+		for der in ders[:-1]:
 			if der > max_dte or der < 0:
 				skip = True
 				break
